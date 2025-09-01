ffbaa92f50290052f3084ea7a445b4d30aca5e31
devbatra2004 <dbatra767@gmail.com>
2025-09-01 10:57:06 +0530

    final

--- first_program.java
+++ first_program.java
@@ -1,6 +1,4 @@
-
-
-class Main {
+class New {
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
--- sum_Of_Numbers.java
+++ sum_Of_Numbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+public class sum_Of_Numbers {
     public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
 
